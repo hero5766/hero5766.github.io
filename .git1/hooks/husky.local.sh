887d4cb718b23bf66d9153eff9785acbefebc4a6
hero576 <812041531@qq.com>
2020-10-07 22:19:34 +0800

    vision

--- .git1/hooks/husky.local.sh
+++
@@ -1,6 +0,0 @@
-# Created by Husky v4.2.5 (https://github.com/typicode/husky#readme)
-#   At: 2020-6-21 16:58:07
-#   From: undefined (https://github.com/typicode/husky#readme)
-
-packageManager=npm
-cd "."
